a55b57b1d13af9de15019db373456f3d5fa4fe45
Shrikant Kamble <shrikant.kamble@northgateps.com>
2021-04-09 13:59:16 +0530

    first commit

---
+++ SimpleAPI.Tests/Tests/ValuesControllerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleAPI.Tests.Tests
+{
+    using SimpleAPI.Controllers;
+    using Xunit;
+
+    public class ValuesControllerTests
+    {
+        private readonly ValuesController controller;
+        public ValuesControllerTests()
+        {
+            this.controller = new ValuesController();
+        }
+
+        //[Fact]
+        //public void WhenIndexCalled_ShouldReturn_Values()
+        //{
+        //    Assert.Equal(, this.controller.Get());
+        //}
+
+        [Fact]
+        public void WhenGetWithIdCalled_Returns_ActualDetailsForId()
+        {
+            Assert.Equal("this", this.controller.Get(1).Value);
+        }
+    }
+}
---
+++ SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value 1", "value 2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "this";
+        }
+    }
+}
